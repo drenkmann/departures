47d466a18e54d18118976f5d03758b75602366f1
drenkmann <l.vondrenkmann@yahoo.com>
2024-12-01 16:19:23 +0100

    feat: switch to location_on icon for home

--- lib/main.dart
+++ lib/main.dart
@@ -125,8 +125,8 @@ class _AppMainPageState extends State<AppMainPage> {
         },
         destinations: <Widget>[
           NavigationDestination(
-            icon: const Icon(Icons.near_me_outlined),
-            selectedIcon: const Icon(Icons.near_me),
+            icon: const Icon(Icons.location_on_outlined),
+            selectedIcon: const Icon(Icons.location_on),
             label: appLocalizations.navigationLabelHome,
           ),
           NavigationDestination(
